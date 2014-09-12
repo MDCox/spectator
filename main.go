4016bb9e7df5daddf3e8f42de00f49547ea67961
Matt Cox <mattcoxpdx@gmail.com>
2014-09-12 11:56:48 -0700

    Main package skeleton

---
+++ main.go
@@ -0,0 +1,10 @@
+// Collects, parses, and displays Portland IRC community data.
+
+package main
+
+// Handles all the different packages to make sure the data-collecting bot
+// can input it's data into Neo4j, and that the http server has no problem
+// querying that data.
+func main() {
+
+}
