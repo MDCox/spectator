2815cb23481086181ddaf7504b332e80ac7c8f2a
Matt Cox <mattcoxpdx@gmail.com>
2014-09-12 15:14:16 -0700

    Connects to freenode

--- irc/irc.go
+++ irc/irc.go
@@ -1,2 +1,20 @@
 // Handles irc connections and logs to be sent to the parser
 package irc
+
+import (
+	"fmt"
+	"github.com/sorcix/irc"
+	"log"
+)
+
+func Connect(hostname string) {
+	//channels := []string{"#bottesting"}
+
+	fmt.Printf("connection to %s\n", hostname)
+	c, err := irc.Dial(hostname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	msg, _ := c.Decode()
+	fmt.Println(msg)
+}
--- main.go
+++ main.go
@@ -2,9 +2,15 @@
 
 package main
 
+import (
+	"./irc"
+	"fmt"
+)
+
 // Handles all the different packages to make sure the data-collecting bot
 // can input it's data into Neo4j, and that the http server has no problem
 // querying that data.
 func main() {
-
+	fmt.Println("Server starting...")
+	irc.Connect("irc.freenode.net:6665")
 }
