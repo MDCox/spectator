3803ed659db8b8570df197dd0bbdca6871953074
Matt Cox <mattcoxpdx@gmail.com>
2014-09-12 17:58:00 -0700

    Connects to Neo4j

--- data/handle.go
+++ data/handle.go
@@ -4,14 +4,30 @@ package data
 
 import (
 	"fmt"
-	//"github.com/jmcvetta/neoism"
+	"github.com/jmcvetta/neoism"
+	"github.com/sorcix/irc"
+	"log"
 )
 
-func Handle(msg string) {
-	fmt.Println(msg)
+func ConnectToNeo() *neoism.Database {
+	db, err := neoism.Connect("http://localhost:7474/db/data")
+	if err != nil {
+		log.Fatal("COULD NOT CONNECT TO NEO4J: ", err)
+	}
+	return db
 }
 
-func format(msg string) {
+func Handle(input string, db *neoism.Database) {
+	msg := irc.ParseMessage(input)
+	if msg == nil {
+		fmt.Println("Could not parse message")
+	}
+
+	format(msg)
+}
+
+func format(msg *irc.Message) {
+
 }
 
 func store() {
--- main.go
+++ main.go
@@ -12,14 +12,19 @@ import (
 // can input it's data into Neo4j, and that the http server has no problem
 // querying that data.
 func main() {
+	// Channel to be used to pass information back from irc server
 	c := make(chan string)
 	var msg string
+
+	// Connect to Neo4j server
+	db := data.ConnectToNeo()
+
 	fmt.Println("Server starting...")
 	go irc.Connect("irc.freenode.net:6665", c)
 	for {
 		select {
 		case msg = <-c:
-			data.Handle(msg)
+			data.Handle(msg, db)
 		}
 	}
 }
