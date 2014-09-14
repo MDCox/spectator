ae88bdadb3c0c8a6cb58b28a9abd335a343452c8
Matt Cox <mattcoxpdx@gmail.com>
2014-09-14 14:16:42 -0700

    Add store by action documentation as outline

--- data/store.go
+++ data/store.go
@@ -4,18 +4,23 @@ import (
 	"github.com/sorcix/irc"
 )
 
+// Create user node and "IS_IN" edge to room if non-existent.
 func joined(msg *irc.Message) {
 
 }
 
-func parted(msg *irc.Message) {
-
-}
-
+// Check if reference to a person associated with that room.
+// If it is, create a "REFERENCED" edge between speaker and
+// the reference.  If that edge already exists, increment
+// the "times" property by 1.
 func action(msg *irc.Message) {
 
 }
 
+// Check if reference to a person associated with that room.
+// If it is, create a "REFERENCED" edge between speaker and
+// the reference.  If that edge already exists, increment
+// the "times" property by 1.
 func pmsged(msg *irc.Message) {
 
 }
